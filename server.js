f672a5036177a6767f4fd757efaafcac22322ff2
liam-mack <liammma9@gmail.com>
2020-11-01 10:44:12 -0500

    Heroku Connection Fix

--- server.js
+++ server.js
@@ -19,14 +19,6 @@ if (process.env.JAWSDB_URL) {
 }
 
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_Pass,
-    database: "employee_trackerDB"
-});
-
 connection.connect((err) => {
     if (err) throw err;
 
